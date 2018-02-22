df49858e7050e5d7a1a24adc181d50df8bfbb744
common <common.stdyn@gmail.com>
2018-02-22 10:21:28 +0800

    revised igsrv with config file, imugps.cfg

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill igsrv
 sleep 10
-LD_LIBRARY_PATH=lib bash -c "./gpsd /dev/ttyUSB0"
-LD_LIBRARY_PATH=lib bash -c "./igsrv -F -L -T1.0 -S5 -k7 -p19 "
+LD_LIBRARY_PATH=lib bash -c "./gpsd /dev/gps_device"
+LD_LIBRARY_PATH=lib bash -c "./igsrv"
--- start_ptl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sleep 10
-LD_LIBRARY_PATH=lib bash -c "./gpsd /dev/gps_device"
-LD_LIBRARY_PATH=lib bash -c "./igsrv -L -T1.0 -S5 -k7 -p1"
